e3244081074ce5b52b30e7e51e7f22e047839f10
yanyin1986 <yanyin1986@gmail.com>
2017-02-28 15:51:49 +0900

    first commit

--- WebKyu/AppDelegate.swift
+++ WebKyu/AppDelegate.swift
@@ -12,10 +12,11 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var time: NSDate?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        time = NSDate()
         return true
     }
 
--- WebKyu/ViewController.swift
+++ WebKyu/ViewController.swift
@@ -7,19 +7,71 @@
 //
 
 import UIKit
+import WebKit
+import SnapKit
 
 class ViewController: UIViewController {
-
+    
+    var _webView: UIWebView?
+    @IBOutlet weak var _webContainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    func loadUrl(url: URL) {
+        if _webView == nil {
+            _webView = UIWebView(frame: self.view.bounds)
+            _webView!.backgroundColor = UIColor.white
+            _webView?.scrollView.backgroundColor = UIColor.white
+            
+            let top: CGFloat
+            if let navBarHeight = self.navigationController?.navigationBar.frame.height,
+                navBarHeight > 0 {
+                let statusBarHeight = UIApplication.shared.statusBarFrame.height
+                top = navBarHeight + statusBarHeight
+                _webView?.scrollView.clipsToBounds = false
+            } else {
+                top = 0
+            }
+            let bottom = 50
+            
+            _webContainer.addSubview(_webView!)
+            _webView!.snp.makeConstraints({ (make) in
+                make.leading.equalTo(_webContainer.snp.leading)
+                make.trailing.equalTo(_webContainer.snp.trailing)
+                make.top.equalTo(_webContainer.snp.top).offset(top)
+                make.bottom.equalTo(_webContainer.snp.bottom).offset(-bottom)
+            })
+            
+            
+        }
+        _webView!.loadRequest(URLRequest(url: url))
+    }
+}
 
-
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if var path = textField.text {
+            if !path.hasPrefix("http://") && !path.hasPrefix("https://") {
+                path = "http://" + path
+            }
+            
+            if let url = URL(string: path) {
+                self.loadUrl(url: url)
+            }
+        }
+        textField.resignFirstResponder()
+        return true
+    }
 }
 

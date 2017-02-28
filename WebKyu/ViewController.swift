97fbbc1a9d25957523d4e4ca3135bf7794c95b61
yanyin1986 <yanyin1986@gmail.com>
2017-02-28 14:28:55 +0900

    Initial Commit

---
+++ WebKyu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebKyu
+//
+//  Created by yan on 2017/02/28.
+//  Copyright © 2017 mmd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

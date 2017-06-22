9672298c6416baeeb342109f41e4fd0409739d3d
yanyin1986 <yanyin1986@gmail.com>
2017-06-22 11:02:34 +0900

    add FavIcon to get fav.icon for ios

---
+++ WebKyu/FavIcon/libXML2/WebKyu-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import "HTMLDocument.h"
+#import "HTMLElement.h"
+#import "XMLDocument.h"
+#import "XMLElement.h"
